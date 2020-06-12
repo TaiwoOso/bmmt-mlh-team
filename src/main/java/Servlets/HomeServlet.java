159a24d309fa16bbd2372c2f6844f0aae7ca2ffd
John Amiscaray <john_ernest_amiscaray@outlook.com>
2020-06-12 13:36:20 -0400

    initialization

---
+++ src/main/java/Servlets/HomeServlet.java
@@ -0,0 +1,32 @@
+package Servlets;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/covid.do")
+public class HomeServlet extends HttpServlet{
+
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		//forward to our home.jsp upon entry to our page
+		request.getRequestDispatcher("/WEB-INF/Home.jsp").forward(request, response);
+			
+	}//doGet
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		
+			
+	}//doGet
+	
+	
+}//ENDOFCLASS
